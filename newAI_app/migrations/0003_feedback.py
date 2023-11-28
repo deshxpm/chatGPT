2d05caab5f6c1c94ba04e52e61a2ce807c758673
deshxpm <desh.deepak@signimus.com>
2023-11-29 00:38:03 +0530

    updated29112023

--- newAI_app/migrations/0003_feedback.py
+++ newAI_app/migrations/0003_feedback.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='FeedBack',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'))
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('resion', models.CharField(max_length=100)),
                 ('remark', models.TextField()),
                 ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
